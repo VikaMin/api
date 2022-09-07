51f9af4c6cbd9f28b6b1f1b384ac275ca620384a
VikaMin <4673813@gmail.com>
2022-09-08 00:00:36 +0300

    first commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday;
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчества
+    public String phone; // поле для номера телефона
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для выбра получать информацию о скидках в виде sms
+
+}
